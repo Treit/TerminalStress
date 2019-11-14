dfc051e7a5c5dab3aa1999dc376af8dc2db0c1f9
Mike Treit <mtreit@ntdev.microsoft.com>
2019-11-13 16:33:14 -0800

    Initial commit of a simple terminal stress program.

---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TerminalStress
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string s = string.Empty;
+
+            while (true)
+            {
+                using (var fs = new FileStream(@"c:\temp\repro.txt", FileMode.Create))
+                using (var sw = new StreamWriter(fs, Encoding.Unicode))
+                {
+                    char c = (char)r.Next(0xD100, 0xFA95);
+                    s += c;
+
+                    sw.WriteLine(s);
+                    sw.Flush();
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
