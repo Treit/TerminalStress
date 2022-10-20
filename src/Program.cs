3fa832e9bc1a701ed2b3e02a8bbfaeee76d178cf
Mike Treit <mtreit@ntdev.microsoft.com>
2022-10-20 14:31:29 -0700

    Latest version.

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace TerminalStress
 {
     using System;
+    using System.Diagnostics;
     using System.IO;
     using System.Text;
 
@@ -8,22 +9,45 @@
     {
         static void Main(string[] args)
         {
+
             Random r = new Random();
 
-            Console.OutputEncoding = Encoding.UTF8;
-
+#pragma warning disable SYSLIB0001
+            Console.OutputEncoding = args.Length > 0 ? Encoding.UTF7 : Encoding.UTF8;
             string s = string.Empty;
 
             while (true)
             {
-                char c = (char)r.Next(0xD100, 0xFA95);
+                try
+                {
+                    Console.SetCursorPosition(r.Next(Console.WindowWidth), r.Next(Console.WindowHeight));
+                }
+                catch
+                {
+                    Console.Write("☠️");
+                }
+
+                char c = (char)r.Next(0, 0xFFFF);
+                Console.Write(c);
                 s += c;
 
-                Console.WriteLine(s);
+                if (r.Next(1_000) == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine(s);
+                }
 
-                if (s.Length > 100_000)
+                if (s.Length > 1_000)
                 {
                     s = string.Empty;
+                }
+
+                if (r.Next(1_000_000) < 100)
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Console.Write("☠️");
+                    }
                 }
             }
         }
